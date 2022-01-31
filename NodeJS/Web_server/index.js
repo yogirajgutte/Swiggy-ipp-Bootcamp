35dc08ba40bc08cdc48865b4fafae919a0e649c2
yogirajgutte <gutteyogiraj@gmail.com>
2022-01-31 16:33:57 +0530

    Renamed folder

--- NodeJS/Web_server/index.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-
-http
-  .createServer((req, res) => {
-    res.write("<h1>Hello World</h1>");
-  })
-  .listen(8080);
--- Problem Statement 3 - Stack Overflow/models/userModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Users", userSchema);
